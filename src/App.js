1005121ea71bf480be183d12a5184d4b0425a5b2
coach77777 <108881946+coach77777@users.noreply.github.com>
2022-11-15 03:43:00 -0600

    userResults  moved to Context

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import About from './pages/About';
 import Home from './pages/Home';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar'
+import {GithubProvider} from './context/github/GithubContext'
 
 
 function App() {
   return (
+    <GithubProvider>
     <Router> 
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
@@ -22,6 +24,7 @@ function App() {
         <Footer />
       </div>
     </Router>
+    </GithubProvider>
   )
 }
 
