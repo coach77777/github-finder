a54febe73713c0ed3d5a69fcab4f20536d30d24f
coach77777 <108881946+coach77777@users.noreply.github.com>
2022-11-16 03:53:38 -0600

    end L58 sexn 9

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,28 +8,34 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
-    users: [],
+    users: [ ],
     loading: false,
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
 
-//Get initial for testing purposes
-  const fetchUsers = async () => {
+//Get search results
+  const searchUsers = async (text) => {
     setLoading()
 
-    const response = await fetch(`${GITHUB_URL}/users`, {
+    const params = new URLSearchParams({
+      q: text,
+    })
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
 
-    const data = await response.json()
+    const { items } = await response.json()
+
+
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     })
   }
 
@@ -37,7 +43,9 @@ export const GithubProvider = ({ children }) => {
     dispatch({ type: "SET_LOADING" });
   }
 
-
+//Clear users from state
+const clearUsers = ( ) => {dispatch({type:  'CLEAR_USERS' });
+}
   // //Set LoaDING
   // const setLoading = () => dispatch({ type: SET_LOADING })
 
@@ -46,7 +54,8 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers,
       }}
     >
       {children}
